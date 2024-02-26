32b65d6d91716c8d03091ddde203e29ac081c36f
Jonathan Drumond <jonathandrumond03@gmail.com>
2024-02-26 18:30:56 -0300

    Removendo defines inuteis

--- lib/I2C/I2C.c
+++ lib/I2C/I2C.c
@@ -1,22 +1,6 @@
 #include "I2C.h"
 #include "esp_log.h"
 
-
-
-#define I2C_MASTER_SCL_IO 22               /*!< gpio number for I2C master clock */
-#define I2C_MASTER_SDA_IO 21               /*!< gpio number for I2C master data  */
-#define I2C_MASTER_FREQ_HZ 100000        /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
-#define SLAVE_ADDRESS 0x0A
-
-#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
-#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
-#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
-#define ACK_VAL 0x0                             /*!< I2C ack value */
-#define NACK_VAL 0x1           
-
 int i2c_init(int SDA_PIN, int SCL_PIN, int FREQ, i2c_mode MODE)
 {
     if(MODE == I2C_MASTER_MODE)
